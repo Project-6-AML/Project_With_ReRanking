f37f61178a3de3a11544e721cfdd8e578b2eae3d
Critaek <serra.cri2000@gmail.com>
2023-06-15 10:31:37 +0200

    Commit

--- RRT_SOP/utils/training.py
+++ RRT_SOP/utils/training.py
@@ -175,31 +175,31 @@ def train_rerank_backbone(model: nn.Module,
 
         if len(features) >= save_size:
             features_to_save = torch.cat(features, 0)
-            print(f"features_to_save dimension: {features_to_save.size()}")
-            print(f"Tensor to save size: {features_to_save.nelement() * features_to_save.element_size() / 1048576} MB")
+            #print(f"features_to_save dimension: {features_to_save.size()}")
+            #print(f"Tensor to save size: {features_to_save.nelement() * features_to_save.element_size() / 1048576} MB")
             torch.save(features_to_save, f"/content/Project_With_ReRanking/RRT_SOP/data/features_{save_order}.pt")
             save_order += 1
-            print(f"Free GPU memory before deleting: {get_gpu_memory()}")
+            #print(f"Free GPU memory before deleting: {get_gpu_memory()}")
             del features
             del features_to_save
             torch.cuda.empty_cache()
             features = []
-            print(f"Free GPU memory after deleting: {get_gpu_memory()}")
+            #print(f"Free GPU memory after deleting: {get_gpu_memory()}")
 
     scheduler.step()
 
     if len(features) < save_size:
         features_to_save = torch.cat(features, 0)
-        print(f"features_to_save dimension: {features_to_save.size()}")
-        print(f"Tensor to save size: {features_to_save.nelement() * features_to_save.element_size() / 1048576} MB")
+        #print(f"features_to_save dimension: {features_to_save.size()}")
+        #print(f"Tensor to save size: {features_to_save.nelement() * features_to_save.element_size() / 1048576} MB")
         torch.save(features_to_save, f"/content/Project_With_ReRanking/RRT_SOP/data/features_{save_order}.pt")
         save_order += 1
-        print(f"Free GPU memory before deleting: {get_gpu_memory()}")
+        #print(f"Free GPU memory before deleting: {get_gpu_memory()}")
         del features
         del features_to_save
         torch.cuda.empty_cache()
         features = []
-        print(f"Free GPU memory after deleting: {get_gpu_memory()}")
+        #print(f"Free GPU memory after deleting: {get_gpu_memory()}")
 
     return features
 
